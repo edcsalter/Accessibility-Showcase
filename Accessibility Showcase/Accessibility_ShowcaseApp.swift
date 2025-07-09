c429ce25c2d1b5b6bffb785a6b531b37f7ca1a6b
Ed Salter <edcsalter@gmail.com>
2025-07-09 08:58:14 -0400

    Initial Commit

---
+++ Accessibility Showcase/Accessibility_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_ShowcaseApp.swift
+//  Accessibility Showcase
+//
+//  Created by Ed Salter on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility ShowcaseTests/Accessibility_ShowcaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_ShowcaseTests.swift
+//  Accessibility ShowcaseTests
+//
+//  Created by Ed Salter on 7/9/25.
+//
+
+import Testing
+@testable import Accessibility_Showcase
+
+struct Accessibility_ShowcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
